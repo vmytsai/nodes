5a5567dfa53a89980dc98b2d10f54b20f6bb2f6a
Vlad-Mytsai <33195318+Vlad-Mytsai@users.noreply.github.com>
2022-06-29 12:21:56 +0300

    Update gear-update.sh

--- gear-update.sh
+++ gear-update.sh
@@ -7,7 +7,7 @@ echo -e "\n\nUpdating... Please wait :)\n"
 wget https://builds.gear.rs/gear-nightly-linux-x86_64.tar.xz
 sudo tar -xvf gear-nightly-linux-x86_64.tar.xz -C /root
 rm gear-nightly-linux-x86_64.tar.xz
-sudo systemctl restart gear
+sudo systemctl restart gear-node
 
 rm $HOME/gear-update.sh
 
