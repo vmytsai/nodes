681c6e1f55b950d82dde602f30717ab9495fc74f
Vlad-Mytsai <33195318+Vlad-Mytsai@users.noreply.github.com>
2022-06-30 08:34:12 +0300

    Update defund_sn.sh

--- defund_sn.sh
+++ defund_sn.sh
@@ -30,4 +30,4 @@ echo -e "\n\e[40m\e[92mSnapshot updated!!\e[0m\n"
 echo -e "\nChech status: \e[42mcurl localhost:26657/status\e[0m\n"
 echo -e "Chech journal: \e[42mjournalctl -u defund -f -f -o cat\e[0m\n"
 
-
+rm ~/defund_sn.sh 
