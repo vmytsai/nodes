59e200734fca0d7f8a7960571d25a030c0ada53b
Vlad-Mytsai <33195318+Vlad-Mytsai@users.noreply.github.com>
2022-07-27 19:27:05 +0300

    Update sui_update.sh

--- sui_update.sh
+++ sui_update.sh
@@ -4,14 +4,18 @@ sleep 1 && curl -s https://raw.githubusercontent.com/Vlad-Mytsai/nodes/main/logo
 echo -e "\n\e[40m\e[92mSUI node updating... Please wait :)\e[0m\n"
 systemctl stop suid
 sleep 3
-rm -rf $HOME/.sui/db
-wget -qO $HOME/.sui/genesis.blob https://github.com/MystenLabs/sui-genesis/raw/main/devnet/genesis.blob
-cd $HOME/sui
+rm -rf /root/.sui/db/* /root/.sui/genesis.blob /root/sui
+source $HOME/.cargo/env
+cd $HOME
+git clone https://github.com/MystenLabs/sui.git
+cd sui
 git remote add upstream https://github.com/MystenLabs/sui
 git fetch upstream
 git checkout -B devnet --track upstream/devnet
 cargo build --release
-mv $HOME/sui/target/release/{sui,sui-node,sui-faucet} /usr/bin/
+mv ~/sui/target/release/sui-node /usr/local/bin/
+mv ~/sui/target/release/sui /usr/local/bin/
+wget -O /root/.sui/genesis.blob https://github.com/MystenLabs/sui-genesis/raw/main/devnet/genesis.blob
 systemctl restart suid
 sleep 3
 wget -qO-  -t 1 -T 5 --header 'Content-Type: application/json' --post-data '{ "jsonrpc":"2.0", "id":1, "method":"sui_getRecentTransactions", "params":[5] }' "http://127.0.0.1:9000/" | jq
